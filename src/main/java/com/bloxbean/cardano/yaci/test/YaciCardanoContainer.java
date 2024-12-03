8cd5095cde04e263c5f79775b49378a7f88ffedc
Satya <satran004@gmail.com>
2024-12-03 19:59:43 +0800

    Adjust entry point, refactor YACI backend services to use Blockfrost backend service
    Remove custom backend services (BaseService, StoreEpochService, StoreTransactionService, StoreUtxoService) and replace them with Blockfrost service counterparts. Update `YaciCardanoContainer` initialization for various modes and modify test cases to reflect these changes, ensuring compatibility with Blockfrost functionalities.

--- src/main/java/com/bloxbean/cardano/yaci/test/YaciCardanoContainer.java
+++ src/main/java/com/bloxbean/cardano/yaci/test/YaciCardanoContainer.java
@@ -3,13 +3,15 @@ package com.bloxbean.cardano.yaci.test;
 import com.bloxbean.cardano.client.api.ProtocolParamsSupplier;
 import com.bloxbean.cardano.client.api.TransactionProcessor;
 import com.bloxbean.cardano.client.api.UtxoSupplier;
+import com.bloxbean.cardano.client.backend.KupmiosBackendService;
 import com.bloxbean.cardano.client.backend.api.*;
+import com.bloxbean.cardano.client.backend.blockfrost.service.BFBackendService;
+import com.bloxbean.cardano.client.backend.blockfrost.service.BFEpochService;
+import com.bloxbean.cardano.client.backend.blockfrost.service.BFTransactionService;
+import com.bloxbean.cardano.client.backend.blockfrost.service.BFUtxoService;
 import com.bloxbean.cardano.client.backend.kupo.KupoUtxoService;
 import com.bloxbean.cardano.client.backend.ogmios.http.OgmiosEpochService;
 import com.bloxbean.cardano.yaci.test.api.helper.YaciTestHelper;
-import com.bloxbean.cardano.yaci.test.backend.store.StoreEpochService;
-import com.bloxbean.cardano.yaci.test.backend.store.StoreTransactionService;
-import com.bloxbean.cardano.yaci.test.backend.store.StoreUtxoService;
 import com.bloxbean.cardano.yaci.test.backend.ogmios.TestOgmiosTransactionService;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
@@ -30,7 +32,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class YaciCardanoContainer extends GenericContainer<YaciCardanoContainer> {
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("bloxbean/yaci-cli");
-    private static final String DEFAULT_TAG = "0.0.20-beta1";
+    private static final String DEFAULT_TAG = "0.10.0-preview2";
     public static final int STORE_PORT = 8080;
     public static final int CLUSTER_HTTP_PORT = 10000;
     public static final int SUBMIT_API_PORT = 8090;
@@ -48,6 +50,10 @@ public class YaciCardanoContainer extends GenericContainer<YaciCardanoContainer>
 
     public YaciCardanoContainer() {
         this(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
+    }
+
+    public YaciCardanoContainer(String tag) {
+        this(DEFAULT_IMAGE_NAME.withTag(tag));
     }
 
     public YaciCardanoContainer(final DockerImageName dockerImageName) {
@@ -78,8 +84,20 @@ public class YaciCardanoContainer extends GenericContainer<YaciCardanoContainer>
     }
 
     private void init() {
+        addEnv("yaci_cli_mode", "native");
+        addEnv("yaci_store_mode", "native");
+
+        addEnv("conwayHardForkAtEpoch", "1");
+        addEnv("shiftStartTimeBehind", "true");
+
+        //Update entry point to use yaci-cli directly
+        withCreateContainerCmdModifier(cmd -> {
+            cmd.withEntrypoint("/app/yaci-cli");
+        });
+
         if (apiMode == ApiMode.YACI_STORE) {
             addEnv("yaci_store_enabled", "true");
+            addEnv("ogmios_enabled", "true");
 
             waitingFor(Wait.forHttp("/api/v1/epochs/parameters")
                     .forPort(STORE_PORT)
@@ -88,7 +106,8 @@ public class YaciCardanoContainer extends GenericContainer<YaciCardanoContainer>
                     .withStartupTimeout(Duration.ofSeconds(waitTimeout)));
             withStartupTimeout(Duration.ofSeconds(waitTimeout));
         } else if (apiMode == ApiMode.OGMIOS) {
-            addEnv("ogmios.enabled", "true");
+            addEnv("ogmios_enabled", "true");
+            addEnv("kupo_enabled", "true");
             waitingFor(Wait.forHttp("/")
                     .forPort(OGMIOS_PORT)
                     .forStatusCode(200)
@@ -199,7 +218,7 @@ public class YaciCardanoContainer extends GenericContainer<YaciCardanoContainer>
     public EpochService getEpochService() {
         switch (apiMode) {
             case YACI_STORE:
-                return new StoreEpochService(getYaciStoreApiUrl(), "dummy key");
+                return new BFEpochService(getYaciStoreApiUrl(), "dummy key");
             case OGMIOS:
                 return new OgmiosEpochService(getOgmiosHttpUrl());
             default:
@@ -211,7 +230,7 @@ public class YaciCardanoContainer extends GenericContainer<YaciCardanoContainer>
     public UtxoService getUtxoService() {
         switch (apiMode) {
             case YACI_STORE:
-                return new StoreUtxoService(getYaciStoreApiUrl(), "dummy key");
+                return new BFUtxoService(getYaciStoreApiUrl(), "dummy key");
             case OGMIOS:
                 return new KupoUtxoService(getKupoUrl());
             default:
@@ -223,9 +242,21 @@ public class YaciCardanoContainer extends GenericContainer<YaciCardanoContainer>
     public TransactionService getTransactionService() {
         switch (apiMode) {
             case YACI_STORE:
-                return new StoreTransactionService(getYaciStoreApiUrl(), "dummy key");
+                return new BFTransactionService(getYaciStoreApiUrl(), "dummy key");
             case OGMIOS:
                 return new TestOgmiosTransactionService(getOgmiosHttpUrl(), getKupoUrl());
+            default:
+                throw new IllegalArgumentException("Invalid ApiMode : " + apiMode);
+        }
+    }
+
+    @NotNull
+    public BackendService getBackendService() {
+        switch (apiMode) {
+            case YACI_STORE:
+                return new BFBackendService(getYaciStoreApiUrl(), "dummy key");
+            case OGMIOS:
+                return new KupmiosBackendService(getOgmiosHttpUrl(), getKupoUrl());
             default:
                 throw new IllegalArgumentException("Invalid ApiMode : " + apiMode);
         }
--- src/test/java/com/bloxbean/cardano/yaci/test/YaciContainerTest.java
+++ src/test/java/com/bloxbean/cardano/yaci/test/YaciContainerTest.java
@@ -1,15 +1,28 @@
 package com.bloxbean.cardano.yaci.test;
 
 import com.bloxbean.cardano.client.account.Account;
+import com.bloxbean.cardano.client.address.AddressProvider;
 import com.bloxbean.cardano.client.api.ProtocolParamsSupplier;
 import com.bloxbean.cardano.client.api.UtxoSupplier;
+import com.bloxbean.cardano.client.api.model.Amount;
 import com.bloxbean.cardano.client.api.model.Result;
+import com.bloxbean.cardano.client.api.model.Utxo;
 import com.bloxbean.cardano.client.backend.api.TransactionService;
 import com.bloxbean.cardano.client.common.model.Networks;
 import com.bloxbean.cardano.client.function.Output;
 import com.bloxbean.cardano.client.function.TxBuilder;
 import com.bloxbean.cardano.client.function.TxBuilderContext;
 import com.bloxbean.cardano.client.function.helper.MintCreators;
+import com.bloxbean.cardano.client.function.helper.ScriptUtxoFinders;
+import com.bloxbean.cardano.client.function.helper.SignerProviders;
+import com.bloxbean.cardano.client.plutus.blueprint.PlutusBlueprintUtil;
+import com.bloxbean.cardano.client.plutus.blueprint.model.PlutusVersion;
+import com.bloxbean.cardano.client.plutus.spec.BytesPlutusData;
+import com.bloxbean.cardano.client.plutus.spec.ConstrPlutusData;
+import com.bloxbean.cardano.client.plutus.spec.PlutusData;
+import com.bloxbean.cardano.client.plutus.spec.PlutusScript;
+import com.bloxbean.cardano.client.quicktx.QuickTxBuilder;
+import com.bloxbean.cardano.client.quicktx.ScriptTx;
 import com.bloxbean.cardano.client.transaction.spec.Asset;
 import com.bloxbean.cardano.client.transaction.spec.MultiAsset;
 import com.bloxbean.cardano.client.transaction.spec.Policy;
@@ -40,6 +53,7 @@ public class YaciContainerTest {
     private static TransactionService transactionService;
 
     private static YaciCardanoContainer cardanoContainer = new YaciCardanoContainer();
+
     private static YaciTestHelper testHelper = cardanoContainer.getTestHelper();
 
     @BeforeAll
@@ -185,6 +199,45 @@ public class YaciContainerTest {
         System.out.println(result);
         assertThat(testHelper.lovelaceBalance(receiverAddress).get()).isEqualTo(adaToLovelace(2.1));
         assertThat(testHelper.amounts(receiverAddress)).hasSize(1);
+    }
+
+    @Test
+    void lockUnlockTest() throws Exception {
+        String receiver = "addr_test1qz3s0c370u8zzqn302nppuxl840gm6qdmjwqnxmqxme657ze964mar2m3r5jjv4qrsf62yduqns0tsw0hvzwar07qasqeamp0c";
+        String compiledCode = "590169010100323232323232323225333002323232323253330073370e900118049baa0011323232533300a3370e900018061baa005132533300f00116132533333301300116161616132533301130130031533300d3370e900018079baa004132533300e3371e6eb8c04cc044dd5004a4410d48656c6c6f2c20576f726c642100100114a06644646600200200644a66602a00229404c94ccc048cdc79bae301700200414a2266006006002602e0026eb0c048c04cc04cc04cc04cc04cc04cc04cc04cc040dd50051bae301230103754602460206ea801054cc03924012465787065637420536f6d6528446174756d207b206f776e6572207d29203d20646174756d001616375c0026020002601a6ea801458c038c03c008c034004c028dd50008b1805980600118050009805001180400098029baa001149854cc00d2411856616c696461746f722072657475726e65642066616c736500136565734ae7155ceaab9e5573eae855d12ba401";
+
+        PlutusScript plutusScript = PlutusBlueprintUtil.getPlutusScriptFromCompiledCode(compiledCode, PlutusVersion.v3);
+        String scrtiptAddr = AddressProvider.getEntAddress(plutusScript, Networks.testnet()).toBech32();
+
+        //create datum
+        PlutusData datum = ConstrPlutusData.of(0, BytesPlutusData.of(account.getBaseAddress().getPaymentCredentialHash().get()));
+
+        testHelper.lockFund(account, scrtiptAddr, List.of(Amount.ada(10)), datum);
+
+        Thread.sleep(2000);
+
+        //Unlock
+        QuickTxBuilder quickTxBuilder = new QuickTxBuilder(cardanoContainer.getBackendService());
+
+        Utxo scriptUtxo = ScriptUtxoFinders.findFirstByInlineDatum(utxoSupplier, scrtiptAddr, datum).orElseThrow();
+
+        PlutusData redeemer = ConstrPlutusData.of(0, BytesPlutusData.of("Hello, World!"));
+
+        ScriptTx sctipTx = new ScriptTx()
+                .collectFrom(scriptUtxo, redeemer)
+                .payToAddress(receiver, Amount.ada(10))
+                .attachSpendingValidator(plutusScript);
+
+        Result<String> result1 = quickTxBuilder.compose(sctipTx)
+                .feePayer(receiver)
+                .collateralPayer(account.baseAddress())
+                .withSigner(SignerProviders.signerFrom(account))
+                .withRequiredSigners(account.getBaseAddress())
+                .completeAndWait(System.out::println);
+
+        assertThat(result1.isSuccessful()).isTrue();
+        assertThat(testHelper.lovelaceBalance(receiver).get()).isGreaterThanOrEqualTo(adaToLovelace(9));
+
     }
 
     @AfterAll
